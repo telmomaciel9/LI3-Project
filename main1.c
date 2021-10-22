e6356565cc6e57c798cce1d99fb20e1af326e11c
bia14 <bearibeiromonteiro@gmail.com>
2021-10-22 19:00:22 +0100

    Main da aula exercício

---
+++ main1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc, char* argv[]){
+	printf ("Program name : %s\n", argv[0]);
+		if (argc != 2){
+			printf("Wrong nr of arguments");
+		}
+		else{
+			if(strcmp(argv[1], "exercicio-1") == 0){
+				ex1();
+			}
+			else
+				if(strcmp(argv[1], "exercicio-2") == 0){
+					ex2();
+				}
+		}
+		return 0;
+}
