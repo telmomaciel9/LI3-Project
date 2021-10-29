39bfd39cf46032b3217af6add3448c8859ddd7c0
telmomaciel9 <telmomaciel9@gmail.com>
2021-10-29 19:35:06 +0100

    Comparaçao de listas

--- users.c
+++ users.c
@@ -7,6 +7,7 @@ struct user init_user(char *info) {
     //printf("chegou até aqui");
     struct user k;
     //k.public_repos = atoi(strsep(&info, ";"));
+    //id;login;type;created_at;followers;follower_list;following;following_list;public_gists;public_repos
     int a = sscanf(info,"%m[0-9];%m[^;];%m[a-zA-Z];%m[\\-0-9 :];%d;%m[^;];%d;%m[^;];%d;%d",
         &k.id,
         &k.login,
@@ -19,8 +20,8 @@ struct user init_user(char *info) {
         &k.public_gists,
         &k.public_repos);
 //314442; mitchellshelton; User;2010-06-25 11:13:20;9;
-//[272939, 1504500, 1031373, 7852646, 11890867, 3768562, 7041191, 6673982, 4955664];9;
-//[518371, 1031373, 1504500, 2364769, 3768562, 4515014, 4955664, 6305433, 7852646];3;27
+//9;[272939, 1504500, 1031373, 7852646, 11890867, 3768562, 7041191, 6673982, 4955664];9;
+//[518371, 1031373, 1504500, 2364769, 3768562, 4515014, 4955664, 6305433, 7852646];3;27;
 
     if (a == 10) {
         printf("%d %s %s \n",a, k.id,k.login);
@@ -46,6 +47,40 @@ void show_user(struct user k){
     //        k.following_list, 
     //        k.public_gists, 
     //        k.public_repos);
+}
+
+
+int follow_confirm (struct user k){
+    int following_tam, follower_tam;
+
+    //[518371, 1031373, 1504500]
+
+    //comparar lista de followings
+
+    if (k.following_list[1]=="]") following_tam=0;
+    else following_tam=1;
+
+    for (i=0; i++;k.following_list[i]=="]"){
+        if (k.following_list[i]==",") following_tam++;
+    }
+
+    //comparar lista de followers
+
+    if (k.follower_list[1]=="]") follower_tam=0;
+    else follower_tam=1;
+
+    for (i=0; i++;k.follower_list[i]=="]"){
+        if (k.follower_list[i]==",") follower_tam++;
+    }
+
+
+    //compara o tamanho das listas com o valor dado
+
+    if (follower_tam == followers) {
+        if (following_tam == following) return 1;
+    }
+
+    return 0;
 }
 
 //6611157;lorraine94588;User;2014-02-07 01:01:35;0;[];0;[];0;0
